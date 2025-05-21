e0642f8826ce955777788ea47d10cebd0f3737f1
kartiksathwara <kartiksathwara64@gmail.com>
2025-05-21 12:24:30 +0530

    Lock Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Customers from "./components/Customers";
@@ -18,6 +19,15 @@ function App() {
         <Route path="/inventory" element={<Inventory />} />
       </Routes>
     </BrowserRouter>
+=======
+import Lock from "./components/Lock";
+
+function App() {
+  return (
+    <>
+     <Lock />
+    </>
+>>>>>>> 51f7f0c (Lock Page)
   )
 }
 
