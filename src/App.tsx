c782ff49ec2c0db02ba1b45b02de0aac19ef3718
kush-1223 <patelkushdipakkumar@gmail.com>
2025-05-21 11:10:58 +0530

    homepage ui done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
+import HomePage from "./components/HomePage";
+import Customers from "./components/Customers";
 
 function App() {
-  return <></>;
+  return (
+   
+    <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />}></Route>
+          <Route path="/customer" element={<Customers />}/>
+        </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
