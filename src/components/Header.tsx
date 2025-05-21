d57af663f853a3a328e9fae3878bee8f67365a29
kush-1223 <patelkushdipakkumar@gmail.com>
2025-05-21 17:31:43 +0530

    header change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
 const Header = () => {
 	return (
 		<div>
-			<header className="bg-(--main) w-full p-5 py-3 text-2xl flex justify-between">
+			<header className="bg-(--main) w-full p-8 py-3 font-semibold text-2xl flex justify-between">
 				<div className="text-white">DKC</div>
 				<div className="text-white">Profile</div>
 			</header>
