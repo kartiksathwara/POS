12ecef34a4a208fe9662db40ed769ae36999762b
kartiksathwara <kartiksathwara64@gmail.com>
2025-05-23 12:59:29 +0530

    Cancel Order Page-1

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,19 @@ import HomePage from "./components/HomePage";
 import Customers from "./components/Customers";
 import Lock from "./components/Lock";
 import DiscountPage from "./components/DiscountPage";
+import CancelOrder from "./components/CancelOrder";
 
 function App() {
   return (
-   
+
     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />}></Route>
-          <Route path="/customer" element={<Customers />}/>
-          <Route path="/lock" element={<Lock />}/>
-          <Route path="/discount" element={<DiscountPage />} />
-        </Routes>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/customer" element={<Customers />} />
+        <Route path="/lock" element={<Lock />} />
+        <Route path="/discount" element={<DiscountPage />} />
+        <Route path="/cancel-order" element={<CancelOrder />} />
+      </Routes>
     </BrowserRouter>
   )
 }
