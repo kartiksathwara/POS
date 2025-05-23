a58714b4f29e6c4cd1137f9d3b8f40a5b0178316
kush-1223 <patelkushdipakkumar@gmail.com>
2025-05-23 16:35:07 +0530

    requestinventory ui done

--- src/components/InventoryList.tsx
+++ src/components/InventoryList.tsx
@@ -2,7 +2,7 @@ const InventoryList = () => {
 	return (
 		<div>
 			<div className="grid grid-cols-2 px-7 py-2 my-4 gap-6 overflow-y-auto">
-			<div className="flex justify-between items-center p-3 rounded-xl h-20">
+			<div className="flex justify-between items-center p-3 bg-(--bgorder) rounded-xl h-20">
 				<div className="flex gap-3">
 					<img alt="demo" className="w-16 h-16 rounded-sm border" />
 					<div>
